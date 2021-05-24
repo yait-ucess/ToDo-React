2d71001b2d544b08742a922fb6b03a42f9596670
yait-ucess <bandai28@icloud.com>
2021-05-24 17:09:29 +0900

    分割代入でスッキリさせる JavaScriptでは、color: color;のように、オブジェクトでプロパティ名とその値の名前が 一緒の場合、color,と省略できます。

--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 const ColorfulMessage = (props) => {
+  const { color, children } = props;
   const contentStyle = {
-    color: props.color,
+    color,
     fontSize: "30px"
   };
-  return <p style={contentStyle}>{props.children}</p>;
+  return <p style={contentStyle}>{children}</p>;
 };
 
 export default ColorfulMessage;
