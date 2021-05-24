31311d45b3816047abd1ebd9d2d50d13547cf7ec
yait-ucess <bandai28@icloud.com>
2021-05-24 13:00:31 +0900

    import import = 輸入
    import App from "./App";
    どこからimportしてくるかを明示する。

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
-
-const App = () => {
-  return (
-    <>
-      <h1>Hello</h1>
-      <p>You</p>
-    </>
-  );
-};
+import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
