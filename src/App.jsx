775517e6226c3f079a73d9070614a434c7d3c5e9
yait-ucess <bandai28@icloud.com>
2021-05-24 14:38:08 +0900

    イベントの付け方 onClick={　}部分はJavaScriptとして認識されます。 その中に関数名を書きます。

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const App = () => {
+  const onClickBtn = () => alert();
   return (
     <>
       <h1>Hello</h1>
       <p>You</p>
+      <button onClick={onClickBtn}>ボタン</button>
     </>
   );
 };
