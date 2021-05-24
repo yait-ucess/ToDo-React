b60bf5e77df70fb9235e80ee69b1d7df155c12c8
yait-ucess <bandai28@icloud.com>
2021-05-24 17:28:00 +0900

    State Stateは、各コンポーネントが持っている可変の状態のことを指します。 条件によって動的に変わる部分をStateとして定義することで、 様々な状態の画面を表示していくことができるようになります。
    const [num, setNum] = useState(0);
    
    [　]
    １つ目にStateの変数名、２つ目にStateを更新していく関数名を書きます。
    
    (　)
    初期値を設定できます。

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickBtn = () => alert();
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   return (
     <>
       <h1 style={{ color: "red" }}>Hello</h1>
       <ColorfulMessage color="blue">You</ColorfulMessage>
       <ColorfulMessage color="pink">Are</ColorfulMessage>
-      <button onClick={onClickBtn}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
