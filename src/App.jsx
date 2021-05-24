045d6a0039a784d07ea25f92aa5c71f464ca6ec3
yait-ucess <bandai28@icloud.com>
2021-05-24 14:44:19 +0900

    styleの当て方 <h1 style={{ color: "red" }}>Hello</h1> のように直接書くか、
      const contentStyle = {
        color: "blue",
        fontSize: "30px"
      };
    <p style={contentStyle}>You</p>
    のように変数化してそれを使う。

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React from "react";
 
 const App = () => {
   const onClickBtn = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "30px"
+  };
   return (
     <>
-      <h1>Hello</h1>
-      <p>You</p>
+      <h1 style={{ color: "red" }}>Hello</h1>
+      <p style={contentStyle}>You</p>
       <button onClick={onClickBtn}>ボタン</button>
     </>
   );
