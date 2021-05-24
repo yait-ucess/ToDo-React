363c12bd4668b4ae8d3df217c9fe3f70ff34d93e
yait-ucess <bandai28@icloud.com>
2021-05-24 18:52:14 +0900

    顔文字が消えたり出現したりするボタン

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
   return (
     <>
       <h1 style={{ color: "red" }}>Hello</h1>
       <ColorfulMessage color="blue">You</ColorfulMessage>
       <ColorfulMessage color="pink">Are</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(๑˃̵ᴗ˂̵)</p>}
     </>
   );
 };
