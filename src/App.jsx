901bb2a41c58df3edb9acfe883a24c5a6aeda4f4
yait-ucess <bandai28@icloud.com>
2021-05-25 11:35:03 +0900

    styleマークアップ

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import "./style.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="ToDoを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のToDO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のToDO</p>
         <ul>
           <li>
-            <div>
+            <div className="list-row">
               <p>快感回路</p>
               <button>完了</button>
               <button>削除</button>
             </div>
-            <div>
+            <div className="list-row">
               <p>人生は20代で決まる</p>
               <button>完了</button>
               <button>削除</button>
@@ -25,11 +25,11 @@ export const App = () => {
           </li>
         </ul>
       </div>
-      <div>
-        <p>完了のToDO</p>
+      <div className="complete-area">
+        <p className="title">完了のToDO</p>
         <ul>
           <li>
-            <div>
+            <div className="list-row">
               <p>確率論的思考</p>
               <button>戻す</button>
             </div>
@@ -38,4 +38,4 @@ export const App = () => {
       </div>
     </>
   );
-}
+};
