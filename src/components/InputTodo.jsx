ba5f155c6f217bb8e025602bb3f4f7601203a127
yait-ucess <bandai28@icloud.com>
2021-05-26 17:35:49 +0900

    CSS in JS CSSの記述を、コンポーネントファイルに移す。

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#c1ffff",
+  width: "auto",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+  display: "flex",
+  justifyContent: "space-between"
+};
+
 export const InputTodo = (props) => {
   const { todoText, onChange, onClick } = props;
   return (
-    <div className="input-area">
+    <div style={style}>
       <input placeholder="ToDoを入力" value={todoText} onChange={onChange} />
       <button onClick={onClick}>追加</button>
     </div>
