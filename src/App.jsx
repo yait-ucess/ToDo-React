89751b4a7b3bddeed7eced07d2c8ec25443f1200
yait-ucess <bandai28@icloud.com>
2021-05-26 17:01:12 +0900

    inputエリアのコンポーネント化 親コンポーネントのApp.jsxから、 propsとして必要な関数などを渡します。

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./style.css";
+import { InputTodo } from "./components/inputTodo";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
@@ -42,14 +43,11 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="ToDoを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
       <div className="incomplete-area">
         <p className="title">未完了のToDO</p>
         <ul>
