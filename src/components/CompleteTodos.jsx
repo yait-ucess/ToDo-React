28afd0e304f8500b4153bd27e4cc5dc65d00996f
yait-ucess <bandai28@icloud.com>
2021-05-26 17:24:12 +0900

    完了エリアのコンポーネント化

--- src/components/CompleteTodos.jsx
+++ src/components/CompleteTodos.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const CompleteTodos = (props) => {
-  const {} = props;
+  const { todos, onclickBack } = props;
   return (
     <div className="complete-area">
       <p className="title">完了のToDO</p>
       <ul>
         <li>
-          {completeTodos.map((todo, index) => {
+          {todos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <p>{todo}</p>
